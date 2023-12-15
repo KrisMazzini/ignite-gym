b0e3d037152eef5b24455dff705c45cd10fc9a73
Kristopher Mazzini <kristophermazzini.sc@gmail.com>
2023-12-15 07:53:14 -0300

    feat(auth): create SignIn and SignUp screens

--- App.tsx
+++ App.tsx
@@ -1,35 +1,29 @@
 /* eslint-disable camelcase */
-import { StatusBar, Text, View } from 'react-native'
-
+import { StatusBar } from 'react-native'
+import { NativeBaseProvider } from 'native-base'
 import {
   useFonts,
   Roboto_400Regular,
   Roboto_700Bold,
 } from '@expo-google-fonts/roboto'
 
+import { THEME } from './src/theme'
+
+import { SignUp } from '@screens/SignUp'
+import { Loading } from '@components/Loading'
+
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold })
 
   return (
-    <View
-      style={{
-        flex: 1,
-        alignItems: 'center',
-        justifyContent: 'center',
-        backgroundColor: '#202024',
-      }}
-    >
+    <NativeBaseProvider theme={THEME}>
       <StatusBar
         barStyle="light-content"
         backgroundColor="transparent"
         translucent
       />
 
-      {fontsLoaded ? (
-        <Text style={{ color: 'white' }}>Hello World</Text>
-      ) : (
-        <View />
-      )}
-    </View>
+      {fontsLoaded ? <SignUp /> : <Loading />}
+    </NativeBaseProvider>
   )
 }
